8a4776349d1259cedc6072e3ef75c5239b97d71e
Lobo-Iberico <Lobo-Iberico@users.noreply.github.com>
2021-01-31 22:51:03 +0100

    Player Movement Script 0.1

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,18 +1,123 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Va a guardar los input
+    #region Variables
+    PlayerMovement playerMovement;
+
+    public bool inverse = false; // Invertir la direccion del golpe
+
+    public float mouseInUseTime = 0.1f; // Cuanto tiempo debe estar el raton parado para ignorarlo.
+    public float forceDistance = 0.1f;
+
+    float mouseInUse = 0;
+    bool active = true;
+
+    Vector3 impulseDirection = Vector3.zero;
+    float impulseForce = 0;
+    #endregion
+
+    #region Inicialicar
+    private void Start()
     {
-        
+        playerMovement = GetComponent<PlayerMovement>();
+        playerMovement.BallStopped.AddListener(Active);
+        playerMovement.BallShot.AddListener(Desactive);
+    }
+    #endregion
+
+    #region Inputs
+    public void Active()
+    {
+        active = true;
+    }
+    public void Desactive()
+    {
+        active = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetMouseDelta(InputAction.CallbackContext context)
     {
-        
+        mouseInUse = mouseInUseTime;
     }
+    #endregion
+
+    #region Procesos
+    private void Update()
+    {
+        if (active)
+        {
+            if (mouseInUse > 0) mouseInUse -= Time.deltaTime;
+            CalculateAddres();
+        }
+    }
+
+    void CalculateAddres()
+    {
+        if (mouseInUse > 0)
+        {
+            impulseDirection = MouseDirection();
+            impulseForce = MouseImpulse();
+        }
+
+        SetLine();
+    }
+
+    Vector3 MouseDirection()
+    {
+        Vector2 ballDir = Camera.main.WorldToScreenPoint(transform.position);
+        Vector2 mouseDir = Mouse.current.position.ReadValue();
+        Vector3 direction;
+
+        if (inverse) direction = (ballDir - mouseDir).normalized;
+        else direction = (mouseDir - ballDir).normalized;
+
+        return new Vector3(direction.x, 0, direction.y);
+    }
+
+    float MouseImpulse()
+    {
+        Vector2 ballDir = Camera.main.WorldToScreenPoint(transform.position);
+        Vector2 mouseDir = Mouse.current.position.ReadValue();
+
+        float force = Vector2.Distance(ballDir, mouseDir) * forceDistance;
+        force = Mathf.Clamp(force, 0, playerMovement.maxForce);
+
+        return force;
+    }
+
+    void SetLine()
+    {
+        Vector3 posA, posB;
+
+        if (!inverse)
+        {
+            posA = transform.position;
+            posB = transform.position + (impulseDirection * impulseForce);
+        }
+        else
+        {
+            posA = transform.position + (impulseDirection * impulseForce);
+            posB = transform.position;
+        }
+
+        playerMovement.DrawLine(posA, posB);
+    }
+    #endregion
+
+    #region Outputs
+
+    public void Shot(InputAction.CallbackContext context)
+    {
+        if (active)
+        {
+            playerMovement.ShotBall(impulseDirection, impulseForce);
+        }
+    }
+    #endregion
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Estado de la bola
+    // Va a leer los inputs y a mover la bola
+
+    #region Variables
+    Rigidbody rigi;
+    LineRenderer line;
+
+    public float forceMult = 1.5f;
+    public float maxForce = 5;
+
+    public UnityEvent BallStopped;
+    public UnityEvent BallShot;
+
+    bool stoped = true;
+
+    #endregion
+
+    #region Inicialicar
+    private void Start()
     {
-        
+        rigi = GetComponent<Rigidbody>();
+        line = GetComponent<LineRenderer>();
+    }
+    #endregion
+
+    #region Inputs
+    #endregion
+
+    #region Procesos
+    private void Update()
+    {
+        if (rigi.velocity.magnitude <= 0.3f && !stoped)
+        {
+            print("STOP");
+            rigi.velocity = Vector3.zero;
+            BallStopped.Invoke();
+            line.enabled = true;
+            stoped = true;
+        }
+    }
+    public void ShotBall(Vector3 direction, float force)
+    {
+        rigi.AddForce(direction * force * forceMult, ForceMode.Impulse);
+
+        BallShot.Invoke();
+        line.enabled = false;
+
+        stoped = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DrawLine(Vector3 posA, Vector3 posB)
     {
-        
+        line.SetPosition(0, posA);
+        line.SetPosition(1, posB);
     }
+    #endregion
+
+    #region Outputs
+    #endregion
 }
