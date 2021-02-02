f94d63f70da18e3fddb95dcee6e57caac62e83b0
programmerNacho <56942092+programmerNacho@users.noreply.github.com>
2021-02-02 22:32:33 +0100

    Sound prototype

---
+++ Assets/Scripts/PlayerSounds.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSounds : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerMovement playerMovement = null;
+    [SerializeField]
+    private float maxPitchHit = 1f;
+    [SerializeField]
+    private float minPitchHit = 0f;
+
+    private AudioSource fxAudioSource = null;
+
+    [SerializeField]
+    private AudioClip hitSound = null;
+
+    private void Start()
+    {
+        fxAudioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if(playerMovement.currentShotState == PlayerMovement.ShotState.Release)
+        {
+            fxAudioSource.pitch = Mathf.Lerp(minPitchHit, maxPitchHit, Random.Range(0f, 1f));
+            fxAudioSource.PlayOneShot(hitSound);
+        }
+    }
+}
